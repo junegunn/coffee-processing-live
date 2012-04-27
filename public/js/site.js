3f33bf151b4d4ad1e56a8c2ee4c400a52dd5b587
Junegunn Choi <junegunn.c@gmail.com>
2012-04-27 10:07:41 +0900

    resumable

--- public/js/site.js
+++ public/js/site.js
@@ -7,6 +7,7 @@ $(function() {
       type: 'POST',
       data: { code: $("#left textarea").val() },
       success: function(data) {
+        if (window.p5) window.p5.exit()
         eval(data)
         window.p5 = new Processing($("canvas")[0], this.sketch)
       }
@@ -19,15 +20,19 @@ $(function() {
       type: 'POST',
       data: { code: $("#left textarea").val() },
       success: function(data) {
-        window.p5.noLoop()
-        eval(data)
-        this.sketch(window.p5)
-        window.p5.loop()
+        if (window.p5) {
+          window.p5.noLoop()
+          eval(data)
+          this.sketch(window.p5)
+          window.p5.loop()
+        } else {
+          window.p5 = new Processing($("canvas")[0], this.sketch)
+        }
       }
     })
   })
 
   $("#stop").click(function() {
-    if (window.p5) window.p5.exit()
+    if (window.p5) window.p5.noLoop()
   })
 })
